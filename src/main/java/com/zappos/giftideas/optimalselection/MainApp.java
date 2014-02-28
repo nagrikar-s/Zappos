253554cf8e1dcc58d3d97a16dfdf127367f627e9
Sneha <nagrikar.s@husky.neu.edu>
2014-02-28 15:47:00 -0800

    initial commit

---
+++ src/main/java/com/zappos/giftideas/optimalselection/MainApp.java
@@ -0,0 +1,13 @@
+package com.zappos.giftideas.optimalselection;
+
+public class MainApp {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.println("null");
+    }
+
+}
